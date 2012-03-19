bda0ca01ecec643452fe9a873914994d42ebb41a
Cedric Porter <cedricporter@gmail.com>
2012-03-20 00:11:12 +0800

    高亮FTP返回码

---
+++ 格式化代码/color return code.py
@@ -0,0 +1,13 @@
+﻿inf = open("1.txt", "r")
+text = ""
+for line in inf.readlines():
+    try:
+        i = int(line[:2])
+        line = line[:3] + "</font></strong>" + line[3:]
+        line = '<strong><font color="#f79646">' + line
+    except:
+        pass
+    text += line
+print text
+out = open("out.txt", 'w')
+out.write(text)
