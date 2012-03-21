a324831e732c58c92a009d54faa57b2d20d1ad9c
Cedric Porter <cedricporter@gmail.com>
2012-03-21 20:14:04 +0800

    修改了格子生成

--- image/cell/make.py
+++ image/cell/make.py
@@ -8,6 +8,6 @@ for i in range(52):
     for j in range(51):
         count += 1
         if count % 2 == 0:
-            draw.rectangle((i * 10, j * 10, (i+1) * 10, (j+1)*10),fill="#ff0000")
+            draw.rectangle((i * 10, j * 10, (i+1) * 10, (j+1)*10),fill="#ffff00")
 
 im.save('test.bmp')
