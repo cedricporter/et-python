97030fe90dffb8329cfd5b9afa70f1634d1de728
Cedric Porter <cedricporter@gmail.com>
2012-03-21 19:34:39 +0800

    生成格子

---
+++ image/cell/make.py
@@ -0,0 +1,13 @@
+﻿import Image, ImageDraw
+
+im = Image.open('1.bmp')
+draw = ImageDraw.Draw(im)
+count = 0
+
+for i in range(52):
+    for j in range(51):
+        count += 1
+        if count % 2 == 0:
+            draw.rectangle((i * 10, j * 10, (i+1) * 10, (j+1)*10),fill="#ff0000")
+
+im.save('test.bmp')
