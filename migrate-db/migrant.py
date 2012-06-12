83e9700b11649480e9ea67c38d7f2c52315ef711
Cedric Porter <cedricporter@gmail.com>
2012-06-12 16:14:28 +0800

    escaped = true

--- migrate-db/migrant.py
+++ migrate-db/migrant.py
@@ -16,37 +16,37 @@ sql = sys.stdin.read()
 #sql = open('et.sql').read()
 
 # [python] => <pre lang="python">
-sql = sub(r'\[python.*?\]', sql, '<pre lang="python">')
+sql = sub(r'\[python.*?\]', sql, '<pre lang="python" escaped="true">')
 sql = sub(r'\[/python\]', sql, '</pre>')
 
-sql = sub(r'\[plain.*?\]', sql, '<pre lang="text">')
+sql = sub(r'\[plain.*?\]', sql, '<pre lang="text" escaped="true">')
 sql = sub(r'\[/plain\]', sql, '</pre>')
 
-sql = sub(r'\[code.*?\]', sql, '<pre lang="text">')
+sql = sub(r'\[code.*?\]', sql, '<pre lang="text" escaped="true">')
 sql = sub(r'\[/code\]', sql, '</pre>')
 
-sql = sub(r'(\[c(\s.*?)?\])', sql, '<pre lang="cpp">')
+sql = sub(r'(\[c(\s.*?)?\])', sql, '<pre lang="cpp" escaped="true">')
 sql = sub(r'\[/c\]', sql, '</pre>')
 
-sql = sub(r'\[shell.*?\]', sql, '<pre lang="bash">')
+sql = sub(r'\[shell.*?\]', sql, '<pre lang="bash" escaped="true">')
 sql = sub(r'\[/shell\]', sql, '</pre>')
 
-sql = sub(r'\[php.*?\]', sql, '<pre lang="php">')
+sql = sub(r'\[php.*?\]', sql, '<pre lang="php" escaped="true">')
 sql = sub(r'\[/php\]', sql, '</pre>')
 
-sql = sub(r'\[lisp.*?\]', sql, '<pre lang="lisp">')
+sql = sub(r'\[lisp.*?\]', sql, '<pre lang="lisp" escaped="true">')
 sql = sub(r'\[/lisp\]', sql, '</pre>')
 
-sql = sub(r'\[javascript.*?\]', sql, '<pre lang="javascript">')
+sql = sub(r'\[javascript.*?\]', sql, '<pre lang="javascript" escaped="true">')
 sql = sub(r'\[/javascript\]', sql, '</pre>')
 
-sql = sub(r'\[html.*?\]', sql, '<pre lang="html">')
+sql = sub(r'\[html.*?\]', sql, '<pre lang="html" escaped="true">')
 sql = sub(r'\[/html\]', sql, '</pre>')
 
-sql = sub(r'\[c\+\+.*?\]', sql, '<pre lang="cpp">')
+sql = sub(r'\[c\+\+.*?\]', sql, '<pre lang="cpp" escaped="true">')
 sql = sub(r'\[/c\+\+\]', sql, '</pre>')
 
-sql = sub(r'\[c#.*?\]', sql, '<pre lang="csharp">')
+sql = sub(r'\[c#.*?\]', sql, '<pre lang="csharp" escaped="true">')
 sql = sub(r'\[/c#\]', sql, '</pre>')
 
 # for s in re.findall(r'\<pre lang=\".*?\"\>', sql):
