d5e06a0f26b2383010bce6d6950b7c8d846fb3c4
Cedric Porter <cedricporter@gmail.com>
2013-03-31 13:27:01 +0800

    arp

--- arp/arpkill.py
+++ arp/arpkill.py
@@ -25,30 +25,7 @@ def kill(targets, gateway_ip="192.168.1.1", nloop=True):
 
 if __name__ == '__main__':
 
-    #kill('192.168.0.107', '192.168.0.1')
-    targets = ['192.168.0.141',
-     '192.168.0.163',
-     '192.168.0.125',
-     '192.168.0.134',
-     '192.168.0.153',
-     '192.168.0.136',
-     '192.168.0.137',
-     '192.168.0.145',
-     '192.168.0.121',
-     '192.168.0.132',
-     '192.168.0.123',
-     '192.168.0.117',
-     '192.168.0.107',
-     '192.168.0.1',
-     '192.168.0.255',
-     '192.168.0.113',
-     '192.168.0.110',
-     '192.168.0.101',
-     '192.168.0.152',
-     '192.168.0.109',
-     '192.168.0.108']
-
-    targets = ['125.216.227.' + str(ip) for ip in range(255) if ip != 11]
+    # targets = ['125.216.227.' + str(ip) for ip in range(255) if ip != 11]
 
     targets = sys.stdin.read().splitlines()
 
@@ -57,6 +34,9 @@ if __name__ == '__main__':
     gateway = filter(is_gateway, targets)
     targets = filter(lambda x: not is_gateway(x), targets)
     targets = filter(lambda x: len(x) != 0, targets)
+
+    if not gateway:
+        gateway = targets[0][:targets[0].rfind('.') + 1] + "254"
 
     print gateway
     print targets
